c645938a839376256437b2aa50245c626401421a
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-25 16:35:02 -0800

    fix UI functionality

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -20,42 +20,35 @@ namespace PierresBakery
       Console.WriteLine("~~~~~~~~~~~~~~~~~~");
       Console.WriteLine("Pastries are 1 for $2 or 3 for $5!");
       Console.WriteLine("~~~~~~~~~~~~~~~~~~");
-      Console.WriteLine("~~~~~~~~~~~~~~~~~~");
       Console.WriteLine("Thank you for shopping with us!  Type anything to continue..}");
       Console.WriteLine("To leave at anytime.   type:{EXIT}");
       string input = Console.ReadLine();
+      int pastryAmount = 0;
+      int breadAmount = 0;
       while(input != "EXIT")
       {
         Console.WriteLine("Would you like some pastries, bread, or directions to the door?    type:{pastry/bread/leave}");
         input = Console.ReadLine();
-        int pastryAmount = 0;
-        int breadAmount = 0;
         if (input.ToLower() == "pastry")
         {
           Console.WriteLine("Enter amount of pastries: ");
           pastryAmount += int.Parse(Console.ReadLine());
-          // Console.WriteLine(pastryAmount + " pastryAmount ");
         }
         else if (input.ToLower() == "bread")
         {
           Console.WriteLine("How many loaves of bread would you like: ");
           breadAmount += int.Parse(Console.ReadLine());
-          // Console.WriteLine(breadAmount + " breadAmount ");
         }
         else if (input.ToLower() == "leave")
         {
-          Console.WriteLine(pastryAmount + " pastryAmount ");
-          Console.WriteLine(breadAmount + " breadAmount ");
         Order user = new Order(breadAmount, pastryAmount);
-        Console.WriteLine(user + " user");
         Bread breadOrder= new Bread(user.Bread);
         Pastry pastryOrder = new Pastry(user.Pastry);
         int breadCost = Bread.CostForBread(breadOrder.Loaves);
         int pastryCost = Pastry.CostForPastries(pastryOrder.Pastries);
-        Console.WriteLine(breadCost + " breadCost " + pastryCost + " pastryCost ");
         int total = breadCost + pastryCost;
         Console.WriteLine("Your total is $"+total);
-        Console.WriteLine("Thank you for shopping with us!");
+        Console.WriteLine("Thank you for coming in!");
         input = "EXIT";
         }
       }
